c66dcdd2c7a31b3cce59c3b8a931a8dee1942f95
Recep Salih KILINÇ <recep.kilinc@ogr.sakarya.edu.tr>
2025-12-19 17:45:48 +0300

    Pages için sw.js dosyası eklendi.

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'kutuphane-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/app.js',
+    './js/config.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request)
+            .then((response) => response || fetch(e.request))
+    );
+});
